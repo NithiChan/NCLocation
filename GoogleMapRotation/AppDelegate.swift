07f017eb87830546f69aea6a26a6bfe9a6a861df
NithiChan <nithiyanantham.c@gmail.com>
2017-09-14 18:09:16 +0530

    Update AppDelegate.swift

--- GoogleMapRotation/AppDelegate.swift
+++ GoogleMapRotation/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyAt9kuHewVm4t23zmD5UjQY1oNOqjjCaN0")
+        GMSServices.provideAPIKey("xxxYourAPIKeyxxx")
         // Override point for customization after application launch.
         return true
     }
